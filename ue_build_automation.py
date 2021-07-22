150b0aa96d3365f993e51db0900e9799bd0e71f9
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-07-22 16:10:06 +0300

    add code inspection

--- ue_build_automation.py
+++ ue_build_automation.py
@@ -7,6 +7,7 @@ import git
 import stat
 import platform
 import getopt
+import winreg
 
 # Prerequisites:
 #
@@ -28,9 +29,11 @@ import getopt
 engine_path = None                  # path to UE install directory
 demo_project_path = None            # path to demo project directory
 demo_project_name = None            # demo project name
-build_output_path = None            # path to the directory where build artifacts will be stored
+build_output_path = None            # path to build artifacts direcory
 plugin_repo_link = None             # UE plugin repo link
 plugin_repo_branch = None           # UE plugin repo branch
+inspect_tool_path = None            # path to code inspection tool
+inspect_artifact_path = None        # path to code inspection artifact
 
 # Plugin git clone progress tracking utility
 class CloneProgress(git.remote.RemoteProgress):
@@ -42,21 +45,54 @@ def DeleteReadOnly(action, name, exc):
     os.chmod(name, stat.S_IWRITE)
     os.remove(name)
 
+# File writing utility
+def writeFile(filename, data):
+    with open(filename, 'wb') as f:
+        f.write(data.encode('utf-8'))
+
+# Get custom batch files folder path utility
+def getCustomBatchScriptDir():
+    scriptDir = os.path.join(os.environ['HOMEDRIVE'] + os.environ['HOMEPATH'], '.ue4')
+    try:
+        os.makedirs(scriptDir)
+    except:
+        pass    
+    return scriptDir
+
+# Get project files generation script utility
+def getGenerationScript():           
+    try:
+        # Query the shell integration to determine the location of the UnrealVersionSelector.exe, which generates VS project files
+        key = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, 'Unreal.ProjectFile\\shell\\rungenproj\\command')
+        if key:
+            command = winreg.QueryValue(key, None)
+            if len(command) > 0:                
+                # Write the command to run UnrealVersionSelector.exe to custom batch file
+                customBat = os.path.join(getCustomBatchScriptDir(), 'GenerateProjectFiles.bat')
+                writeFile(customBat, command.replace('"%1"', '%1') + '\r\n')
+                return customBat
+    except:
+        pass
+    
+    raise Exception('Could not detect the location of UnrealVersionSelector.exe.')
+
 # Check script args
-if len(sys.argv) < 6:
+if len(sys.argv) < 8:
     print('Provide the following parameters:')
     print('-u, --ue_base - path to the UE installation directory')
     print('-d, --demo_base - path to the demo project directory')
     print('-n, --demo_name - demo project name')
-    print('-o, --build_output - path to the directory where build artifacts will be stored')
+    print('-o, --build_output - path to build artifacts direcory')
     print('-r, --repo - UE plugin repo link')
     print('-b, --branch - UE plugin repo branch')
+    print('-i, --inspect_tool - path to code inspection tool')
+    print('-a, --inspect_artifact - path to code inspection artifact')
     sys.exit('Invalid parameter provided')
 
 # Parse script args
 argv = sys.argv[1:]
 try:
-    opts, args = getopt.getopt(argv, "u:d:n:o:r:b:", ["ue_base=", "demo_base=", "demo_name=", "build_output=", "repo=", "branch="])
+    opts, args = getopt.getopt(argv, "u:d:n:o:r:b:i:a:", ["ue_base=", "demo_base=", "demo_name=", "build_output=", "repo=", "branch=", "inspect_tool=", "inspect_artifact="])
 except:
     sys.exit('Error: Failed to parse arguments')
 
@@ -73,6 +109,10 @@ for opt, arg in opts:
         plugin_repo_link = arg
     elif opt in ['-b', '--branch']:
         plugin_repo_branch = arg
+    elif opt in ['-i', '--inspect_tool']:
+        inspect_tool_path = os.path.abspath(arg)
+    elif opt in ['-a', '--inspect_artifact']:
+        inspect_artifact_path = os.path.abspath(arg)
 
 # Check whether all variables are set
 if engine_path is None:
@@ -87,6 +127,10 @@ if plugin_repo_link is None:
     sys.exit('Error: Provide a valid UE plugin repo link')
 if plugin_repo_branch is None:
     sys.exit('Error: Provide a valid UE plugin repo branch')
+if inspect_tool_path is None:
+    sys.exit('Error: Provide a valid path to code inspection tool')
+if inspect_artifact_path is None:
+    sys.exit('Error: Provide a valid path to code inspection artifact')
 
 # Check platform for running this script (currently only Windows supported)
 if not platform.system() == 'Windows':
@@ -97,7 +141,7 @@ if not os.path.exists(demo_project_path):
     sys.exit(f'Error: Failed to locate demo project at {demo_project_path}')
 
 # Clear temporaries if any in demo project folder
-temp_folders = ['Binaries', 'Build', 'Intermediate', 'DerivedDataCache', 'Saved', 'Plugins']
+temp_folders = ['Binaries', 'Build', 'Intermediate', 'DerivedDataCache', 'Saved', 'Plugins', '.vs']
 for temp_folder in temp_folders:
     temp_folder_path = os.path.join(demo_project_path, temp_folder)
     if (os.path.exists(temp_folder_path)):
@@ -120,8 +164,8 @@ if os.path.exists(packages_path):
 os.makedirs(packages_path)
 
 # Package demo project
-build_platforms = ['Win64', 'Android']
 demo_project = os.path.join(demo_project_path, demo_project_name + '.uproject')
+build_platforms = ['Win64', 'Android']
 for platform in build_platforms:
     result = subprocess.run([uat,
         'BuildCookRun',
@@ -140,3 +184,17 @@ for platform in build_platforms:
     if result.returncode != 0:
         print(f'Error: AutomationTool Error: {result.stderr}')
         break
+
+# Generate IDE (Visual Studio) project files for demo project
+genScript = getGenerationScript()
+generation = subprocess.run([genScript, demo_project], cwd=os.path.dirname(genScript), stdout=sys.stdout)
+
+# Prepare folder for storing code inspection artifacts
+inspect_path = os.path.join(inspect_artifact_path, 'Inspect')
+if os.path.exists(inspect_path):
+    shutil.rmtree(inspect_path)
+os.makedirs(inspect_path)
+
+# Run code inspection
+demo_project_sln = os.path.join(demo_project_path, demo_project_name + '.sln')
+inspection = subprocess.run([inspect_tool_path, demo_project_sln, '-o=' + os.path.join(inspect_path, 'InspectResult.xml'), '--project=' + demo_project_name], stdout=sys.stdout)
